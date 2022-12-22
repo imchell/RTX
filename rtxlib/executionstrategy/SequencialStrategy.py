7a0ed024bce5cd0d8cc00a147f55da2f5af58166
Chell <im.chell@outlook.com>
2022-12-22 18:34:48 +0100

    implement opti_values and result_values in sequential strategy

--- rtxlib/executionstrategy/SequencialStrategy.py
+++ rtxlib/executionstrategy/SequencialStrategy.py
@@ -2,6 +2,7 @@ from colorama import Fore
 
 from rtxlib import info, error
 from rtxlib.execution import experimentFunction
+from rtxlib.storage import State
 
 
 def start_sequential_strategy(wf):
@@ -9,8 +10,10 @@ def start_sequential_strategy(wf):
     info("> ExecStrategy   | Sequential", Fore.CYAN)
     wf.totalExperiments = len(wf.execution_strategy["knobs"])
     for kn in wf.execution_strategy["knobs"]:
-        experimentFunction(wf, {
-            "knobs":kn,
+        State.opti_values.append(kn["route_random_sigma"])
+        result = experimentFunction(wf, {
+            "knobs": kn,
             "ignore_first_n_results": wf.execution_strategy["ignore_first_n_results"],
             "sample_size": wf.execution_strategy["sample_size"],
         })
+        State.result_values.append(result)
